5c357b12fa6267c3fc30482fd0d2b03144c1bc2f
Mahdi Ashraf(Mahir) <ashraf.mahdi8060@gmail.com>
2026-04-03 20:34:08 +0600

    "feat: add ml-agents 2.x and configure python training environment"

---
+++ Assets/Scripts/Agents/AgentPlaceholder.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+
+public class AgentPlaceholder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public  void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(0f);
+    }
+
+    public void OnActionReceived(ActionBuffers actions)
+    {
+        // Do nothing
+    }
+
+}
---
+++ Dependencies.md
@@ -0,0 +1,14 @@
+# Dependencies
+
+ Component             Version          
+----------------------------------------
+ Unity Editor          2022.3.62f3 LTS    
+ ML-Agents (C#)        2.0.2            
+ mlagents (Python)     0.30.0           
+ mlagents_envs         0.30.0           
+ Python                3.9.23           
+ PyTorch               2.8.0            
+ NumPy                 1.21.2 
+ TensorBoard           2.20.0 
+ Matplotlib            3.5.2           
+ SciPy                 1.7.3           
